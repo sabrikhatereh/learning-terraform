b0e441530759d968d9690368e771a9cd0012ccc8
sabrikhatereh <109426481+sabrikhatereh@users.noreply.github.com>
2025-03-11 15:42:05 +0000

    security group by module

--- main.tf
+++ main.tf
@@ -22,10 +22,24 @@ resource "aws_instance" "belog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "belog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "belog_new"
+
+  vpc_id              = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog"{
