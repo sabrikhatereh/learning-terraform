081cc932266f00ddc1af747004e7093558b8d1a7
sabrikhatereh <109426481+sabrikhatereh@users.noreply.github.com>
2025-03-11 18:12:20 +0000

    fix

--- main.tf
+++ main.tf
@@ -42,7 +42,10 @@ module "autoscaling" {
   min_size = 1
   max_size = 2
 
+
+  
   vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_groups["ex-instance"].arn
   security_groups = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
